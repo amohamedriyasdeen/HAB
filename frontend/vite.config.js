9116d6ea0a7b3a6266557372cda02cde8d0b336d
amriyasdeen <riyasdeen.a@hepl.com>
2026-03-23 14:58:45 +0530

    Config Changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig({
-  base: "./",
-  plugins: [react()],
-  server: {
-    port: import.meta.env.VITE_PORT || 5173,
-  },
-  build: {
-    outDir: 'dist',
-  },
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return defineConfig({
+    base: "./",
+    plugins: [react()],
+    server: {
+      port: Number(env.VITE_PORT) || 5173
+    },
+    build: {
+      outDir: "dist"
+    }
+  })
+}
